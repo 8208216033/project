e915fcede9c3dc023c6da6d98ceb49f0bdc80ec2
8208216033 <punamsontakke11@gmail.com>
2021-07-29 14:00:16 +0530

    commit temporary

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  let myvar= 345;
 
   const incrementCount = () => {
     setCount(count + 1);
   };
 
   return (
+
+
     <div>
+      <div>
+    <h1>Hello</h1>
+  </div>
       <h1>{count}</h1>
+      <h3>{myvar}</h3>
       <input type="button" onClick={incrementCount} />
     </div>
+    
+  
   );
 };
+ 
+
+
+
+
 
 export default App;
